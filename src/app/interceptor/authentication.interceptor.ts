1bab1262a240dcb174a82e60eb8a7a36e46f0e69
Najima Sahloumi <nsahloum@gmail.com>
2022-01-21 16:18:29 +0100

    Sign In - JC, NS - back to codecoach keycloak

--- src/app/interceptor/authentication.interceptor.ts
+++ src/app/interceptor/authentication.interceptor.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {catchError, Observable} from "rxjs";
 import {Router} from "@angular/router";
 import {KeycloakService} from "../service/keycloak.service";
 
@@ -13,30 +13,31 @@ export class AuthenticationInterceptor implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (!this.keycloakService.isLoggedIn()) {
-      this.router.navigateByUrl("/signin").then();
-      return next.handle(req);
+    // if (!this.keycloakService.isLoggedIn()) {
+    //   this.router.navigateByUrl("/signin").then();
+    //   return next.handle(req);
+    // }
+    //
+    // if (this.router.url === '/signin') {
+    //   return next.handle(req);
+    // }
+    if (this.keycloakService.isLoggedIn()){
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.keycloakService.getToken()}`
+        }
+      })
     }
-
-    if (this.router.url === '/signin') {
-      return next.handle(req);
-    }
-
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${this.keycloakService.getToken()}`
-      }
-    })
     return next.handle(req).pipe(
-      //   catchError(
-      //     (err: any) => {
-      //     if (err.status === 403 || err.status === 401) {
-      //       this.router.navigateByUrl("/error").then();
-      //     } else if(err.status === 0) {
-      //       this.router.navigateByUrl("/backend-unavailable").then()
-      //     }
-      //     return new Error(err as unknown as string);
-      //   })
+        // catchError(
+        //   (err: any) => {
+        //   if (err.status === 403 || err.status === 401) {
+        //     this.router.navigateByUrl("/error").then();
+        //   } else if(err.status === 0) {
+        //     this.router.navigateByUrl("/backend-unavailable").then()
+        //   }
+        //   return new Error(err as unknown as string);
+        // })
     );
   }
 }
--- src/app/service/sign-in.service.ts
+++ src/app/service/sign-in.service.ts
@@ -18,9 +18,9 @@ export class SignInService {
     const signInBody = new URLSearchParams();
     signInBody.set('username', signInData.email);
     signInBody.set('password', signInData.password);
-    signInBody.set('client_id', 'order');
+    signInBody.set('client_id', 'CodeCoach-Dwaynians');
     signInBody.set('grant_type', 'password');
-    signInBody.set('client_secret', 'afc74d17-d5e9-49bf-8f59-ac22538505b6')
+    signInBody.set('client_secret', '955ad0e4-455f-4bab-ab35-e78337f426d4')
     let options = {
       headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
     };
