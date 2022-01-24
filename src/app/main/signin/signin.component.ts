8c8ae090079bc462cf6410f3cb860041783d7670
Najima Sahloumi <nsahloum@gmail.com>
2022-01-24 11:25:15 +0100

    Sign In - JC, NS - reset password

--- src/app/main/signin/signin.component.ts
+++ src/app/main/signin/signin.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {KeycloakService} from "../../service/keycloak.service";
 import {Router} from "@angular/router";
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-signin',
@@ -23,6 +24,7 @@ export class SigninComponent implements OnInit {
   }
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private formBuilder: FormBuilder,
     private keycloakService: KeycloakService,
     private route: Router) {
@@ -42,7 +44,10 @@ export class SigninComponent implements OnInit {
         }
       })
     );
-    // this.route.navigate(['profile']).then();
+  }
+
+  redirect(): void {
+    window.open("https://keycloak.switchfully.com/auth/realms/java-oct-2021/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkeycloak.switchfully.com%2Fauth%2Frealms%2Fjava-oct-2021%2Faccount%2F%23%2Fsecurity%2Fsigningin&state=07694915-1e09-430c-b000-993084d08ecf&response_mode=fragment&response_type=code&scope=openid&nonce=20c0bec7-c4b1-4b44-9de6-b5914422be53&code_challenge=Y5R5dxAq6x3vvfLd6_8Jveka5gCr2MrwNhFs8A4ssKw&code_challenge_method=S256", "_blank")
   }
 
 }
