d709207fa826dd7fe4796e315471300e336248a0
enoirhom <emil.noirhomme@student.unamur.be>
2022-01-31 10:44:04 +0100

    Refactor remove unused import

--- src/app/request-session/request-session-routing.module.ts
+++ src/app/request-session/request-session-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {RequestSessionFormComponent} from "./request-session-form/request-session-form.component";
 
 const routes: Routes = [
 
